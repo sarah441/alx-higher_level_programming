41a938e4fc8255a817b309d35f3c61ea18d5beb9
sarah441 <sarahibrahim3477@gmail.com>
2024-02-27 19:04:48 +0200

    ggg

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(100):
+  digit = i // 10
+  mod = i % 10
+  if digit < mod:
+    if i == 89:
+        print("{:02}".format(i))
+    else:
+        print("{:02}, ".format(i), end="")
+
