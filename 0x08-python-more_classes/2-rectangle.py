e57cb6608dee62ffefade4110cdbf54c171d19dc
sarah441 <sarahibrahim3477@gmail.com>
2024-04-10 00:02:55 +0200

    ss

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
 
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
 
